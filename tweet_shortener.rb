854efdf066c4f1d099c3b6fc488e79105926fb0e
Justin Hamlin <justin.hamlin@gmail.com>
2018-03-07 04:05:11 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,22 @@
 # Write your code here.
+def dictionary
+word_hash = {
+  "too": "2",
+  "to": "2",
+  "two":"2",
+  "four": "4",
+  "for": "4",
+  "be": "b",
+  "you": "u",
+  "at": "@",
+  "and": "&"
+}
+end
+
+def word_substituter(tweet)
+  tweet.split.collect do |word|
+    if word == dictionary.keys
+    word = dictionary.values
+    end
+end
+end
